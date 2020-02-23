7625471a403298dca5f5282c83c0417f3d509b48
Michael Coppolino <michael_coppolino@brown.edu>
2020-02-23 14:05:09 -0500

    Pull id, title, length of all channels and save to data.db/channels

--- collect_data.py
+++ collect_data.py
@@ -1,39 +1,50 @@
 import requests
 import sqlite3
 
+DATABASE_PATH = 'data.db'
 TARGET_ATTRS = ['id', 'title', 'length']
-NUM_PAGES = 10
+PER_PAGE = 100
 
-def fetch_channel_data():
-    channels = []
+def channel_request_iterator():
+    """
+    yields a list of channel json data of length PER_PAGE
+    """
 
-    for i in range(2, NUM_PAGES + 2):
-        url = 'http://api.are.na/v2/channels?page=%d&amp;per=10' % i
-        print(i, url)
+    page = 2
+    while True:
+        url = 'http://api.are.na/v2/channels?page=%d&amp;per=%d' % (page, PER_PAGE)
+        print(page, url)
+        page += 1
 
-        channel = requests.get(url).json()['channels']
-        channels.extend(channel)
+        req = requests.get(url)
 
-    return channels
+        if req.status_code == 200:
+            yield req.json()['channels']
+        else:
+            break
 
-def write_channel_data(channels):
-    conn = sqlite3.connect('data.db')
+def fetch_and_write_channel_data():
+    """
+    Opens a connection to DATABASE_PATH, and recieves channel data from
+    channel_request_iterator, extracting attributes in TARGET_ATTRS from json
+    and writing to database
+    """
+    conn = sqlite3.connect(DATABASE_PATH)
     c = conn.cursor()
 
     c.execute('DROP TABLE IF EXISTS channels;')
 
     c.execute('CREATE TABLE channels (id int NOT NULL, title varchar, length int);')
 
-    for channel in channels:
-        keep_data = [value for (key, value) in channel.items() if key in TARGET_ATTRS]
-        c.execute('INSERT INTO channels VALUES (?, ?, ?)', tuple(keep_data))
+    for channel_list in channel_request_iterator(): # yield list of json objects
+        for channel in channel_list: # for each json object in list
 
-    conn.commit()
-
+            keep_data = [value for (key, value) in channel.items() if key in TARGET_ATTRS]
+            c.execute('INSERT INTO channels VALUES (?, ?, ?)', tuple(keep_data))
+            conn.commit()
 
 def main():
-    channels = fetch_channel_data()
-    write_channel_data(channels)
+    fetch_and_write_channel_data()
 
 if __name__ == '__main__':
     main()
