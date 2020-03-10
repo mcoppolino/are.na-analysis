0f7c44f1f08c9cf0fb986ed737c9f645ebfa2605
Michael Coppolino <michael_coppolino@brown.edu>
2020-03-10 00:43:37 -0400

    reorganize data tree to make .zip smaller to allow push

--- collect_data.py
+++ collect_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba2a823f2967c405c381d0d40b260f7e1302c7f36d02e3c325c6e608ee7df545
-size 7277
+oid sha256:078996b0f2aa4507a570369487bda7c472aae08c395cc6db0e43acb7278aa8a0
+size 7262
