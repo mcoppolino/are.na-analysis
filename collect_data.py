f7f273361c10c9edfe00dc00a5796d67e9447984
Michael Coppolino <michael_coppolino@brown.edu>
2020-03-10 00:37:53 -0400

    fetch and write user data via query, write to csv and db

--- collect_data.py
+++ collect_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34ceae40a92bd6ef871894d82131c236c0c30ffadda20ca5ff85105c460adb55
-size 4441
+oid sha256:ba2a823f2967c405c381d0d40b260f7e1302c7f36d02e3c325c6e608ee7df545
+size 7277
