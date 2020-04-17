9b02214899a37f9a71c39f150ff8fd1ac300dfad
Michael Coppolino <michael_coppolino@brown.edu>
2020-04-17 05:00:43 -0400

    first pass matrix decomposition refactor

--- analysis/matrix_decomposition.py
+++ analysis/matrix_decomposition.py
@@ -1,107 +1,128 @@
-import csv
-
 import numpy as np
-import pickle
 from scipy.linalg import svd
-import random
-
 from preprocess import get_collaborators
 
 
 class SVDModel:
-    def __init__(self, channels, collabs, l):
+    def __init__(self, channels, collabs, trunc_len):
         print("Initializing model")
-        self.channels = channels
-        self.collabs = collabs
+        self.channels = channels  # guaranteed sorted
+        self.collaborators = collabs
+        self.trunc_len = trunc_len
 
-        self.M = None
-        self.T = None
-        self.U = None
+        self.collab_dict = {}  # map {user id: index of collaborator in M}
+        self.channel_dict = {}  # map {channel id: index of channel in M}
+
+        self.M = None  # M[i][j] indicates user index i collaborated on channel index j
+        self.T = None  # copy of M with some values removed, used for testing
+
+        self.U = None  # results of SVD
         self.D = None
         self.V = None
-        self.M_hat = None
 
-        self.l = l
+        self.M_hat = None  # M_hat[i][j] indicates the likelihood that user index i should collab on channel index j
 
     def construct_matrix(self):
         print('Constructing M and T')
-        all_collabs = []
-        for collab_list in self.collabs:
-            for collab in collab_list:
-                if collab not in all_collabs:
-                    all_collabs.append(collab)
 
-        self.T = np.zeros((len(self.channels), len(all_collabs)))
+        # construct channel dict (guaranteed sorted, so no issues during visualization)
+        num_channels = 0
+        for channel_id in self.channels:
+            if channel_id not in self.channel_dict:
+                self.channel_dict[channel_id] = num_channels
+                num_channels += 1
+            else:
+                print('Uh oh, channel id %i is not unique in dataset' % channel_id)
 
-        # channel i from self.channels[i] has collaborators self.collaborators[i]
-        indices_of_ones = []
-        for i in range(len(self.channels)):
-            for collab in self.collabs[i]:
-                self.T[i][all_collabs.index(collab)] = 1
-                indices_of_ones.append((i, all_collabs.index(collab)))
+        # validate quality of channel_dict
+        assert(len(self.channels) == num_channels)
+        assert(len(self.channel_dict) == num_channels)
 
-        self.M = self.T.copy()
-        shuffled_indices = indices_of_ones.copy()
-        random.shuffle(shuffled_indices)
-        proportion_to_keep = 0.99
-        small_portion_of_shuffled_indices = shuffled_indices[0:int((1 - proportion_to_keep) * len(shuffled_indices))]
-        for (i, j) in small_portion_of_shuffled_indices:
-            self.M[i][j] = 0
+        # construct collab_dict
+        num_collaborators = 0
+        for collab_list in self.collaborators:
+            for user in collab_list:
+                if user not in self.collab_dict:
+                    self.collab_dict[user] = num_collaborators
+                    num_collaborators += 1
 
+        # remove extra from last iteration
+        num_channels -= 1
+        num_collaborators -= 1
 
-    def regular_SVD(self):
+        # initialize M
+        self.M = np.zeros((num_channels + 1, num_collaborators + 1))
+
+        # populate M
+        for channel, collabs in zip(self.channels, self.collaborators):
+            channel_idx = self.channel_dict[channel]
+            for user in collabs:
+                user_idx = self.collab_dict[user]
+                self.M[channel_idx][user_idx] = 1
+
+        # initialize T
+        self.T = self.M.copy()
+
+        # remove p_remove % of adjacencies
+        p_remove = 0.1
+        x, y = np.where(self.T == 1)
+        num_ones = len(x)
+        num_to_remove = int(num_ones * p_remove)
+        remove = np.random.choice(num_ones, num_to_remove)
+        self.T[x[remove], y[remove]] = 0
+
+    def regular_svd(self):
         print('Applying regular SVD')
         self.U, self.D, self.V = svd(self.M)
         self.D = np.diag(self.D)
 
-    def truncated_SVD(self):
-        print("Applying truncated SVD")
-        m, n = self.M.shape
-        self.U, self.D, self.V = svd(self.M)
-        self.D = np.diag(self.D)
-        self.U = self.U[:, 0:self.l]
-        self.D = self.D[0:self.l, 0:self.l]
-        self.V = self.V[0:self.l, :]
+    def truncated_svd(self):
+        self.regular_svd()
+        print('Truncating U, D, V')
+        self.U = self.U[:, 0:self.trunc_len]
+        self.D = self.D[0:self.trunc_len, 0:self.trunc_len]
+        self.V = self.V[0:self.trunc_len, :]
 
     def calculate_predictions(self):
         print('Calculating predictions')
         self.M_hat = np.matmul(self.U, self.V)
 
     def test(self):
-        print("Testing model")
-        threshold_for_correct_prediction = 0.1
-        should_be_ones = []
-        num_predicted = 0
-        num_correct = 0
-        for i in range(len(self.M)):
-            for j in range(len(self.M[i])):
-                if self.T[i][j] == 1 and self.M[i][j] == 0:
-                    index_tuple = (i, j)
-                    prediction = self.M_hat[i][j]
-                    should_be_ones.append((index_tuple, prediction))
-                    if prediction > threshold_for_correct_prediction:
-                        num_correct += 1
-                    num_predicted += 1
-        total_correct_random_predictions = 0
-        random_non_one_indices = []
-        how_many_random = 10000
-        a = 0
-        while a < how_many_random:
-            random_i = random.randint(0,len(self.T)-1)
-            random_j = random.randint(0,len(self.T[0])-1)
-            if self.T[random_i][random_i] != 1:
-                random_non_one_indices.append((random_i,random_j))
-                a += 1
-        for i, j in random_non_one_indices:
-            if self.M_hat[i][j] > threshold_for_correct_prediction:
-                total_correct_random_predictions += 1
-
-        # print("Here is a list of prediction values for testing (they should be close to 1):")
-        # print(should_be_ones)
-        model_accuracy = num_correct / num_predicted
-        random_accuracy = total_correct_random_predictions / how_many_random
-
-        return model_accuracy, random_accuracy
+        return 0, 0
+        # print("Testing model")
+        # threshold_for_correct_prediction = 0.1
+        # should_be_ones = []
+        # num_predicted = 0
+        # num_correct = 0
+        # for i in range(len(self.M)):
+        #     for j in range(len(self.M[i])):
+        #         if self.T[i][j] == 1 and self.M[i][j] == 0:
+        #             index_tuple = (i, j)
+        #             prediction = self.M_hat[i][j]
+        #             should_be_ones.append((index_tuple, prediction))
+        #             if prediction > threshold_for_correct_prediction:
+        #                 num_correct += 1
+        #             num_predicted += 1
+        # total_correct_random_predictions = 0
+        # random_non_one_indices = []
+        # how_many_random = 10000
+        # a = 0
+        # while a < how_many_random:
+        #     random_i = random.randint(0,len(self.T)-1)
+        #     random_j = random.randint(0,len(self.T[0])-1)
+        #     if self.T[random_i][random_i] != 1:
+        #         random_non_one_indices.append((random_i,random_j))
+        #         a += 1
+        # for i, j in random_non_one_indices:
+        #     if self.M_hat[i][j] > threshold_for_correct_prediction:
+        #         total_correct_random_predictions += 1
+        #
+        # # print("Here is a list of prediction values for testing (they should be close to 1):")
+        # # print(should_be_ones)
+        # model_accuracy = num_correct / num_predicted
+        # random_accuracy = total_correct_random_predictions / how_many_random
+        #
+        # return model_accuracy, random_accuracy
 
     # def generate_collaborator_recommendations(self):
     #     recommendation_threshold = 0.1
@@ -126,15 +147,16 @@ class SVDModel:
 
 
 def main():
-    channels, collabs = get_collaborators("../data/csv/collaborators_with_owners.csv", n=1000)
-    truncated_dim = 200
+    channels, collabs = get_collaborators("../data/csv/collaborators_with_owners.csv", n=100)
+
+    trunc_p = 0.4
+    truncated_dim = int(trunc_p * len(channels))
 
     model = SVDModel(channels, collabs, truncated_dim)
     model.construct_matrix()
-    model.truncated_SVD()
+    model.truncated_svd()
     model.calculate_predictions()
     model_acc, random_acc = model.test()
-    print("Model accuracy: %d\nRandom accuracy: %d\n" % (model_acc, random_acc))
 
     np.save('../data/results/M', model.M)
     np.save('../data/results/T', model.T)
@@ -142,6 +164,8 @@ def main():
     np.save('../data/results/D', model.D)
     np.save('../data/results/V', model.V)
     np.save('../data/results/M_hat', model.M_hat)
+    np.save('../data/results/channel_dict', model.channel_dict)
+    np.save('../data/results/collab_dict', model.collab_dict)
 
 
 if __name__ == '__main__':
--- analysis/preprocess.py
+++ analysis/preprocess.py
@@ -4,12 +4,21 @@ import numpy as np
 
 
 
-def get_collaborators(collab_fp, n = 0):
+def get_collaborators(collab_fp, n = None, min_collabs = 2):
     """
     :param collab_fp: filepath to collaborators + owners csv
     :param n: length of data to return, from top of all data sorted by length of collaborators
+    :param min_collabs: the minimum length of a collaborator list that can be returned
     :return: channel_ids, collaborators of length n
     """
+    if n and n < 1:
+        print('get_collaborators received n < 1, but n must be at least 1. Exiting.')
+        exit(0)
+
+    if min_collabs < 0:
+        print('get_collaborators received min_collabs < 2, but min_collabs must be at least 2. Exiting.')
+        exit(0)
+
     with open(collab_fp, 'r') as f:
         r = csv.reader(f)
         next(r)
@@ -17,20 +26,28 @@ def get_collaborators(collab_fp, n = 0):
         channel_ids = []
         collaborators = []
         for row in r:
+            # convert channel id to int and append to channels list
             channel_id = int(row[0])
             channel_ids.append(channel_id)
 
+            # evaluate string representation of collabs and append to collabs list
             collabs = literal_eval(row[1])
             collaborators.append(collabs)
 
+    # sort channels by number of collaborators
     lengths = [len(collabs) for collabs in collaborators]
     data = zip(channel_ids, collaborators, lengths)
     data = sorted(data, key=lambda x: x[2], reverse=True)
-    data = data[:n]
-    channel_ids, collaborators, _ = zip(*data)
 
+    # limit size of output to passed in n
+    if n:
+        data = data[:n]
+
+    # remove channels with less than min_collabs collaborators
+    data = [item for item in data if item[2] >= min_collabs]
+
+    # sort return by channel id
+    data = sorted(data, key=lambda x: x[0])
+    channel_ids, collaborators, _ = zip(*data)
     return channel_ids, collaborators
 
-    # # This is the proportion of data we want to keep as training data:
-    # split_proportion = 0.98
-    # return split_data(channel_ids, collaborators, split_proportion)
