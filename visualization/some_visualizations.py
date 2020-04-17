d81f3a630093288ca15b4056655aa6f23c9eb8ad
Xavi Loinaz <x.e.loinaz@gmail.com>
2020-04-17 05:41:18 -0700

    crappy viz for how many collaborators for a channel vs. average non-one values

--- visualization/some_visualizations.py
+++ visualization/some_visualizations.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 import scipy.sparse as sparse
 import scipy.io as sio
 import pandas as pd
+import sys
+sys.path.insert(1, '../analysis/')
+from preprocess import get_collaborators
+
+collabs_csv = "../data/csv/collaborators_with_owners.csv"
+channels, collabs = get_collaborators(collabs_csv)
 
 m_hat_matrix = np.load('../data/results/M_hat.npy')
 M = np.load('../data/results/M.npy')
@@ -14,6 +20,8 @@ print(M)
 print(channel_dict)
 print(collab_dict)
 
+
+average_non_one_values = []
 for row_index in range(0, len(m_hat_matrix)):
     sum_for_row = 0
     denominator = 0
@@ -22,5 +30,13 @@ for row_index in range(0, len(m_hat_matrix)):
             sum_for_row += m_hat_matrix[row_index][i]
             denominator += 1
     average_non_one_value = sum_for_row / denominator
-    print("average_non_one_value: ", average_non_one_value)
+    average_non_one_values.append(average_non_one_value)
+    # print("average_non_one_value: ", average_non_one_value)
+
+collab_lens = []
+for collabs_mini_list in collabs:
+    collab_lens.append(len(collabs_mini_list))
+
+plt.scatter(collab_lens, average_non_one_value)
+
 
