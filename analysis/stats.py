3ad34d537236554a92f07cfc33168a4871c908d6
Spencer Small <spencer_small@brown.edu>
2020-04-20 19:27:46 -0700

    Merge branch 'master' of /Users/spencerjsmall/Dropbox/SPRING 2020/ARENA/are.na-analysis with conflicts.

--- analysis/stats.py
+++ analysis/stats.py
@@ -3,7 +3,7 @@ from get_model_data import get_model_data
 from sklearn.metrics import mean_absolute_error, mean_squared_error, accuracy_score
 
 # https://stats.stackexchange.com/questions/28287/evaluating-recommender-systems-with-implicit-binary-ratings-only?fbclid=IwAR0143c6c2pzlLxQD36P2WEYzIR5C2wfWvhKhGd3LmoQagauLdmSw1yzIq0
-def RSCORE(M, M_hat, a=2, t=0.5):
+def RSCORE(M, M_hat, a=5, t=0.5):
     """
     :param M, M_hat: results of model :param a: 'patience of user'
     :param t: the minimum threshold of predictions matrix to recommend a channel to a user
@@ -32,32 +32,56 @@ def RSCORE(M, M_hat, a=2, t=0.5):
     return r_score
 
 
-def RMSE(M, T, T_hat, t=0.5):
-    """
-    Calculates the root mean squared error of the test set, documentation found on page 16 of
-    https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/EvaluationMetrics.TR_.pdf
-    """
-    print('Calculating root mean squared error...')
-    test_set = np.where(M != T)
-    y_pred = np.where(T_hat[test_set] > t, 1, 0)
-    y_true = M[test_set]
-
-    return mean_squared_error(y_true, y_pred)
-
-
-def accuracy(M, T, T_hat, t=0.5):
-    """
-    :param M, T, T_hat: results of model
-    :param t: the minimum threshold of predictions matrix to recommend a channel to a user
-    :return: rec_acc, the proportion of correct recommendations in the test set
-    """
-    print('Calculate testing accuracy...')
-
-    test_set = np.where(M != T)
-    y_pred = np.where(T_hat[test_set] > t, 1, 0)
-    y_true = M[test_set]
-
-    return accuracy_score(y_true, y_pred)
+# def RMSE(M, T, T_hat, t=0.5):
+#     """
+#     Calculates the root mean squared error of the test set, documentation found on page 16 of
+#     https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/EvaluationMetrics.TR_.pdf
+#     """
+#     print('Calculating root mean squared error...')
+#     test_set = np.where(M != T)
+#
+#     r = M[test_set]
+#     r_hat = np.where(T_hat[test_set] > 0.5, 1, 0)  # cast T_hat to [0,1] by comparing to t
+#
+#     sum_diff = np.sum(np.sqrt(r - r_hat))
+#     test_set_size = test_set[0].shape[0]
+#     rmse = np.sqrt(sum_diff / test_set_size)
+#
+#     return rmse
+#
+#
+# def MAE(M, T, T_hat, t=0.5):
+#     """
+#     Calculates the mean absolute error of the test set, documentation found on page 16 of
+#     https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/EvaluationMetrics.TR_.pdf
+#     """
+#     print('Calculating mean average error...')
+#     test_set = np.where(M != T)
+#
+#     r = M[test_set]
+#     r_hat = np.where(T_hat[test_set] > 0.5, 1, 0)  # cast T_hat to [0,1] by comparing to t
+#
+#     sum_diff = np.sum(np.absolute(r - r_hat))
+#     test_set_size = test_set[0].shape[0]
+#     mae = np.sqrt(sum_diff / test_set_size)
+#
+#     return mae
+#
+#
+# def accuracy(M, T, T_hat, t=0.5):
+#     """
+#     :param M, T, T_hat: results of model
+#     :param t: the minimum threshold of predictions matrix to recommend a channel to a user
+#     :return: rec_acc, the proportion of correct recommendations in the test set
+#              non_rec_acc, the proportion of correct non-recommendations in the test set
+#     """
+#     print('Calculate testing accuracy...')
+#     test_set = np.where(M != T)
+#     rec_prediction = T_hat[test_set]
+#     rec_correct = np.count_nonzero(np.where(rec_prediction >= t))
+#     rec_acc = rec_correct / len(rec_prediction)
+#
+#     return rec_acc
 
 
 def normalize_predictions(predictions):
@@ -71,27 +95,33 @@ def normalize_predictions(predictions):
 
 def main():
     # extract data using analysis.get_model_data
-    data = get_model_data('../data/model_full')
+    data = get_model_data('../data/model')
     M = data['M']
     T = data['T']
     M_hat = data['M_hat']
     T_hat = data['T_hat']
 
     # normalize predictions by max value per user
+    M_hat_norm = normalize_predictions(M_hat)
     T_hat_norm = normalize_predictions(T_hat)
 
     # calculate metrics
-    rec_acc = accuracy(M, T, T_hat_norm)
-    rmse = RMSE(M, T, T_hat_norm)
     r = RSCORE(M, M_hat)
+    # rmse = RMSE(M, T, T_hat_norm)
+    # mae = MAE(M, T, T_hat_norm)
+    # rec_acc = accuracy(M, T, T_hat_norm)
 
-    metrics = '''
-        Recommendation Accuracy: %d
-        Root Mean Squared Error: %d
-        R-score: %d
-    ''' % (rec_acc, rmse, r)
+    # print metrics
+    # metrics = '''
+    #     R-score: %d
+    #     Root Mean Squared Error: %d
+    #     Mean Average Error: %d
+    #     Recommendation Accuracy: %d
+    # ''' % (r, rmse, mae, rec_acc)
+    #
+    # print(metrics)
 
-    print(metrics)
+    print(r)
 
 
 if __name__ == '__main__':
